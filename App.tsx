4bbce22bb73ac11f09a075b40a4ecfde918dd03f
Yevhen Liuklian <yevhen.liuklian@healthbookplus.com>
2024-02-02 13:49:55 +0200

    feat: fixed App

--- App.tsx
+++ App.tsx
@@ -1,15 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { useEffect, useState } from "react";
 
 export default function App() {
-  const qwe = "asd";
-
-  const [a, setA] = useState(123);
-  useEffect(() => {
-    console.log(a);
-  }, [a]);
-
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
